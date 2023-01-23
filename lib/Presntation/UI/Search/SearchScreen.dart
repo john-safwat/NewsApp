827289b05c459469c39380999b398ecf0851a083
john safwat <john362sss@gmail.com>
2023-01-23 20:42:47 +0200

    mgmj

--- lib/Presntation/UI/Search/SearchScreen.dart
+++ lib/Presntation/UI/Search/SearchScreen.dart
@@ -13,22 +13,55 @@ class SearchScreen extends StatelessWidget {
         leading: const SizedBox(),
         leadingWidth: 0,
         title: Container(
-          color: Colors.red,
+          height: 45,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            color: Colors.white
+          ),
           width: MediaQ.width,
           child: TextFormField(
+            onChanged: (input){
+
+            },
+            cursorColor: Theme.of(context).primaryColor,
+            style: TextStyle(
+              color: Theme.of(context).primaryColor
+            ),
             decoration: InputDecoration(
               prefixIcon: IconButton(
-                icon: const Icon(Icons.close_rounded),
+                icon: Icon(Icons.close_rounded , color: Theme.of(context).primaryColor),
                 onPressed: (){
                   Navigator.pop(context);
                 },
               ),
               suffixIcon: IconButton(
-                icon: const Icon(Icons.search_rounded),
+                icon: Icon(Icons.search_rounded , color: Theme.of(context).primaryColor,),
                 onPressed: (){
-
                 },
-              )
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(1000),
+                borderSide: BorderSide(
+                  color: Theme.of(context).primaryColor,
+                  width: 2 ,
+                  style: BorderStyle.solid,
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(1000),
+                borderSide: BorderSide(
+                  color: Theme.of(context).primaryColor,
+                  width: 2 ,
+                  style: BorderStyle.solid,
+                ),
+              ),
+              focusColor: Theme.of(context).primaryColor,
+              contentPadding: const EdgeInsets.all(0),
+              hintText: "Search Article",
+              hintStyle: TextStyle(
+                color: Theme.of(context).primaryColor
+              ),
+
             ),
           )
         ),
